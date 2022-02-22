dc3d0fc497d8ec0c2100a469f4d5a9ed5d2b2fe6
Denisius21 <denishka2112@gmail.com>
2022-02-22 15:14:23 +0200

    Add MyArray function constructor with prototyps

--- index.js
+++ index.js
@@ -25,11 +25,38 @@ function MyArrayPrototype() {
   this.map = function (func) {
     let result = new MyArray();
     for (let i = 0; i < this.length; i++) {
-      
+
 
       result.push(func(this[i], i, this));
 
     } return result;
+  }
+
+  this.shift = function () {
+    let result = this[0];
+    for (let i = 0; i < this.length; i++) {
+      this[i] = this[i + 1];
+    } delete this[this.length - 1];
+    if (this.length !== 0) {
+      this.length = this.length - 1;
+    } else {
+      this.length = 0;
+    }
+    return result;
+  }
+
+  this.unshift = function (...elements) {
+    this.length += elements.length;
+    for (let i = this.length - 1; i >= 0; i--) {
+      this[i] = this[i - elements.length];
+    }
+    for (let i = 0; i < elements.length; i++) {
+      this[i] = elements[i];
+    } return this.length;
+
+  }
+  MyArray.isMyArray = function (arg) {
+    return arg instanceof MyArray;
   }
 }
 
@@ -40,7 +67,9 @@ MyArray.prototype = myArrayPrototype;
 const myArray2 = new MyArray();
 myArray2.push(5);
 myArray2.push(12);
-
+myArray2.push(9);
+myArray2.push(129);
+myArray2.push(17);
 
 myArray2.forEach(callback);
 function callback(value, index, arr) {
@@ -49,5 +78,5 @@ function callback(value, index, arr) {
 
 const result = myArray2.map(func);
 function func(value, index, arr) {
-  return (value*2);
+  return (value * 2);
 }
